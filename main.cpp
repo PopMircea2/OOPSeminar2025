e43551ed749aa74790345a9855d49c6e757dbad8
mirceapop <mirciupop@gmail.com>
2025-03-06 19:57:10 +0200

    added main, class and functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-//
-// Created by Mircea Pop on 06.03.2025.
-//
 
+
+#include <iostream>
+
+class Caine {
+  public:
+  bool eat() {
+    std::cout<<"Cainele mananca"<< std::endl;
+    return true;
+  };
+  int getAge() {
+    return age;
+  }
+  int setAge(int nage) {
+    age = nage;
+    return age;
+  }
+private:
+  int age;
+};
 int main(){
-  return 2;
+  Caine d = Caine();
+  d.eat();
+  d.setAge(25);
+  std::cout<<d.getAge();
+  return 0;
 }
