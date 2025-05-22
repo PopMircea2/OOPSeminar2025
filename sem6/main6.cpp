18f6cf3dc794ff54f278f63cbf8bb6fafddf6618
mirceapop <mirciupop@gmail.com>
2025-05-22 19:06:51 +0300

    sem6

--- sem6/main6.cpp
+++ sem6/main6.cpp
@@ -1,7 +1,47 @@
 
 #include "main6.h"
 #include<iostream>
+using namespace std;
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    BiletController B;
+    int opt;
+    do {
+        Ui();
+        cin>>opt;
+        if (opt==1) {
+            int id;
+            std::string film;
+            int rand;
+            int loc;
+            float pret;
+            cout<<"Id:\n";
+            cin>>id;
+            cout<<"Film:\n";
+            cin>>film;
+            cout<<"Rand:\n";
+            cin>>rand;
+            cout<<"Loc:\n";
+            cin>>loc;
+            cout<<"Pret:\n";
+            cin>>pret;
+            B.adaugaBilet(id,film,rand,loc,pret);
+        }
+        else if (opt==2) {
+            std::vector<Bilet> bilete = B.print_all();
+            for (const auto &bilet : bilete) {
+                bilet.print();
+            }
+        }
+        else if (opt==3) {
+            float pret_dorit;
+            cout<<"Pretul dorit:\n";
+            cin>>pret_dorit;
+            std::vector<Bilet> bilete = B.sortBilet(pret_dorit);
+            for (const auto &bilet : bilete) {
+                bilet.print();
+            }
+        }
+    }
+    while (opt !=4);
     return 0;
 }
--- sem6/main6.h
+++ sem6/main6.h
@@ -4,12 +4,74 @@
 
 #ifndef MAIN6_H
 #define MAIN6_H
+#include <iostream>
+#include <string>
 
 
+class Bilet {
+private:
+    int Id;
+    std::string Film;
+    int rand;
+    int loc;
+    float pret;
 
-class main6 {
+public:
+    Bilet(int id, const std::string &film, int rand, int loc, float pret) : Id(id), Film(film), rand(rand), loc(loc), pret(pret) {}
 
+    int getId() const {
+        return Id;
+    }
+    const std::string &getFilm() const {
+        return Film;
+    }
+    int getRand() const {
+        return rand;
+    }
+    int getLoc() const {
+        return loc;
+    }
+    float getPret() const {
+        return pret;
+    }
+    void print() const {
+        std::cout << "ID: " << Id
+             << ", Film: " << Film
+             << ", Rand: " << rand
+             << ", Loc: " << loc
+             << ", Pret: " << pret <<std::endl;
+    }
 };
+
+class BiletController {
+private:
+    std::vector<Bilet> bilete;
+public:
+    void adaugaBilet(int id, std::string film, int rand, int loc, float pret) {
+        Bilet b(id, film, rand, loc, pret);
+        bilete.push_back(b);
+    }
+    std::vector<Bilet> print_all() const {
+        return bilete;
+    }
+    std::vector<Bilet> sortBilet(float pret) const {
+        std::vector<Bilet> resultat;
+        for (const auto &bilet : bilete) {
+            if (bilet.getPret() > pret) {
+                resultat.push_back(bilet);
+            }
+        }
+        return resultat;
+    }
+};
+
+void Ui() {
+    std::cout<<"Alege o optiune: \n"
+            "1. Adauga bilet\n"
+            "2. Afiseaza toate biletele\n"
+            "3. Afiseaza bilete cu pret mai mare decat un anumit pret\n"
+            "4. Iesire\n";
+}
 
 
 
